f555a6525c64288bf5c79a0782eede163cea104b
Natansh14687 <natansh522@gmail.com>
2024-02-18 15:58:20 +0530

    tricky

---
+++ Starting_C++/loop_problems/sum_series.cpp
@@ -0,0 +1,14 @@
+// finding the sum of the series 1-2+3-4.....n
+#include<iostream>
+using namespace std;
+int main() {
+    int n,sum=0;
+    cin >>n;
+    for (int i=1; i<=n; i++){
+        if(i%2==0)  sum = sum - i;
+        else
+        sum = sum + i;
+    }
+    cout << sum << endl;
+    return 0;
+}
