496b3586f28cdb3f4f080db6c3fd32301901cbb7
Natansh14687 <natansh522@gmail.com>
2024-02-21 11:13:03 +0530

    printed + taking input

--- Starting_C++/Pattern_Printing/tempCodeRunnerFile.cpp
+++ Starting_C++/Pattern_Printing/tempCodeRunnerFile.cpp
@@ -1,9 +1,3 @@
-for (int i=n-1; i>0; i--){
-//     for(int j=65; j<=2*i+65; j++){
-//         cout << char(j);
-//     }
-//     for(int j=1; j<=n-i; j++){
-//         cout << " ";
-//     }
-//     cout << endl;
-// }
+ for(int j=1; j<=2; j++){
+//     cout << " ";
+//    }
