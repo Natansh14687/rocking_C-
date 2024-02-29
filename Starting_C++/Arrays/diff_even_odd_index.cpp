251cd590c2da5acfeaec5f30a68b1126a76ad014
Natansh14687 <natansh522@gmail.com>
2024-02-29 19:20:58 +0530

    it was according to indices

--- Starting_C++/Arrays/diff_even_odd_index.cpp
+++ Starting_C++/Arrays/diff_even_odd_index.cpp
@@ -11,7 +11,7 @@ int main() {
     }
     int sum_even=0, sum_odd=0;
     for(int i=0; i<size; i++){
-        if ((i+1)%2==0) sum_even+=array[i];
+        if ((i)%2==0) sum_even+=array[i];
         else sum_odd+=array[i];
     }
     int difference = sum_even - sum_odd;
