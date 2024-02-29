405427b519cbd097573f264b8ccc9f8f53a4a253
Natansh14687 <natansh522@gmail.com>
2024-02-29 12:55:24 +0530

    Starting with arrays and DSA

---
+++ Starting_C++/Arrays/sum_elements.cpp
@@ -0,0 +1,17 @@
+// Calculate the sum of all the elements in the given array.
+#include<iostream>
+using namespace std;
+
+int main() {
+    int array[5];
+    int size = sizeof(array)/sizeof(array[0]);
+    for (int i=0; i<size; i++){
+        cin >> array[i];
+    }
+    int sum =0;
+    for (int i=0; i<size; i++){
+        sum=sum+array[i];
+    }
+    cout << sum;
+    return 0;
+}
