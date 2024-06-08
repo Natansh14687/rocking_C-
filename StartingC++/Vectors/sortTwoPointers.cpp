f204ea7af3636cabc24dda70eeffe2a4e2783021
Natansh14687 <natansh522@gmail.com>
2024-06-08 22:12:37 +0530

    6/8/2024

---
+++ StartingC++/Vectors/sortTwoPointers.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(vector<int>& v){
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+}
+
+void swap(int i, int j, vector<int>& v){
+    while(j>=i){
+        if(v[i]==0) i++;
+        else if(v[j] == 1) j--;
+        else if(v[i] == 1 && v[j] == 0){
+            v[i] = 0;
+            v[j] = 1;
+            j--;
+            i++;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter no. of 0s and 1s : ";
+    cin >> n;
+    vector<int> v;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    swap(0,v.size()-1,v);
+    display(v);
+
+    return 0;
+}
