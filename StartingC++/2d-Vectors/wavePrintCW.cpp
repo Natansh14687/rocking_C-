2762afeaaa543199c6007c9db51f4efa7cc2b605
Natansh14687 <natansh522@gmail.com>
2024-06-15 16:44:23 +0530

    6/15/2024

---
+++ StartingC++/2d-Vectors/wavePrintCW.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> v(n,vector<int> (m));
+    for(int i=0; i<v.size(); i++){
+        for(int j=0; j<v[0].size(); j++){
+            cin >> v[i][j];
+        }
+    }
+    for(int i=v.size()-1; i>=0; i--){
+        if(i%2==0){
+            for(int j=0; j<v[0].size(); j++){
+                cout << v[i][j] << " ";
+            }
+        }else{
+            for(int j=v[0].size()-1; j>=0; j--){
+                cout << v[i][j] << " ";
+            }
+        }
+    }
+    return 0;
+}
