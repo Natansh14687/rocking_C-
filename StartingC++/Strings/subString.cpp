045a0a5627bf81f4d712896e3105b9e904c23844
Natansh14687 <natansh522@gmail.com>
2024-06-17 20:01:02 +0530

    6/17/2024

---
+++ StartingC++/Strings/subString.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "Natansh";
+    cout << s.substr(1) << "\n";
+    cout << s.substr(1,1) << "\n";
+    cout << s.substr(1,4) << "\n";
+    return 0;
+}
