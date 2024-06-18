e4b38d32a5bf262d97e696ce791ff814c3607b58
Natansh14687 <natansh522@gmail.com>
2024-06-18 21:06:04 +0530

    6/18/2024

---
+++ StartingC++/Strings/anagram.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    string t;
+    getline(cin,t);
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    if(s == t) cout << "true";
+    else cout << "false";
+}
