a7262ddee412c91abfaf134c7ca4780820aaa103
Natansh14687 <natansh522@gmail.com>
2024-06-19 23:57:53 +0530

    6/19/2024

---
+++ StartingC++/Strings/stringStream.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "My name is Natansh";
+    stringstream ss(s);
+    string temp;
+    while(ss>>temp){
+        cout << temp << "\n";
+    }
+    return 0;
+}
