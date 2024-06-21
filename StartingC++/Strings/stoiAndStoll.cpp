4060e226e7e96f50522bb1d1e0987804d21acef1
Natansh14687 <natansh522@gmail.com>
2024-06-21 17:51:48 +0530

    6/21/2024

---
+++ StartingC++/Strings/stoiAndStoll.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "1223445";
+    int x = stoi(s);
+    cout << x+1;
+    cout << "\n";
+    string ss = "1424535263635454454";
+    long long xx = stoll(ss);
+    cout << xx;
+    return 0;
+}
