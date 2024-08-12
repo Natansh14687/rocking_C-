388cf1aaa3ac16637d99fe89d96656b269483952
Natansh Khurana <natansh522@gmail.com>
2024-08-12 18:56:26 +0530

    stack

---
+++ StartingC++/stack/displayingElementsInStackTopToBottom.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    st.push(60);
+    stack<int> temp;
+    while(st.size()>0){
+        cout << st.top() << " ";
+        temp.push(st.top());
+        st.pop();
+    }
+    while(temp.size()>0){
+        st.push(temp.top());
+        temp.pop();
+    }
+    return 0;
+}
