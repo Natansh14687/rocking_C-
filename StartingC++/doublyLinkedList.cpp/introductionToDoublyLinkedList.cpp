32a4a7a4ea8b7cf9bafa77ca110136ace7ed3293
Natansh Khurana <natansh522@gmail.com>
2024-08-18 01:19:19 +0530

    StartingWithDoublyLinkedList

---
+++ StartingC++/doublyLinkedList.cpp/introductionToDoublyLinkedList.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void display(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << "\n";
+}
+
+void reverseDisplay(Node* tail){
+    Node* temp = tail;
+    while(temp!=NULL){
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << "\n";
+}
+
+int main(){
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    e->prev = d;
+    d->prev = c;
+    c->prev = b;
+    b->prev = a;
+    display(a);
+    reverseDisplay(e);
+
+    return 0;
+}
