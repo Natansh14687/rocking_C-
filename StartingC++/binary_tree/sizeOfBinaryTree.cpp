a362cf9a67e0d698d697838f0963b774917e31d4
Natansh Khurana <natansh522@gmail.com>
2024-08-26 22:46:18 +0530

    Binary_Tree

---
+++ StartingC++/binary_tree/sizeOfBinaryTree.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val){
+        this->val = val;
+        left = right = NULL;
+    }
+};
+
+int size(TreeNode* root){
+    if(root == NULL) return 0;
+    return 1+size(root->left)+size(root->right);
+}
+
+int main(){
+    TreeNode* a = new TreeNode(1); //root
+    TreeNode* b = new TreeNode(2);
+    TreeNode* c = new TreeNode(3);
+    TreeNode* d = new TreeNode(4);
+    TreeNode* e = new TreeNode(5);
+    TreeNode* f = new TreeNode(6);
+    TreeNode* g = new TreeNode(7);
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    cout << size(a);
+    return 0;
+}
