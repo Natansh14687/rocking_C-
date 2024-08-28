4f1eebe4d3534d122cc180da23f0cf011aff7850
Natansh Khurana <natansh522@gmail.com>
2024-08-28 05:49:07 +0530

    binary-trees

---
+++ StartingC++/binary_tree/numberOfLevels.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val){
+        this->val = val;
+        left = right = NULL;
+    }
+};
+
+int levels(TreeNode* root){
+    if(root==NULL) return 0;
+    return 1+max(levels(root->left),levels(root->right));
+}
+
+int main(){
+    TreeNode* a = new TreeNode(1); //root
+    TreeNode* b = new TreeNode(2);
+    TreeNode* c = new TreeNode(3);
+    TreeNode* d = new TreeNode(4);
+    TreeNode* e = new TreeNode(5);
+    TreeNode* f = new TreeNode(6);
+    TreeNode* g = new TreeNode(7);
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    cout << levels(a);
+    return 0;
+}
