8a66beff5e424c80d4ec40a4da318b1fdba64f8a
Natansh Khurana <natansh522@gmail.com>
2024-12-11 14:41:21 +0530

    Uploading New Files

---
+++ StartingC++/SortingAlgorithms/bubbleSortCode.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {5,4,6,3,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1-i; j++){
+            if(arr[i]>arr[i+1]){
+                
+            }
+        }
+    }
+    return 0;
+}
