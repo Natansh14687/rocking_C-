3b9951c6a6a19f7f7f5392650bc0216a589ad92f
Natansh Khurana <natansh522@gmail.com>
2024-12-25 18:56:41 +0530

    contest problem

--- codeForces_contest/preparingForTheOlympiad.cpp
+++ codeForces_contest/preparingForTheOlympiad.cpp
@@ -6,7 +6,29 @@ int main(){
     while(t--){
         int n;
         cin >> n;
+        vector<int> v(n);
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            int x;
+            cin >> x;
+            v[i] = x;
+        }
+        for(int i=0; i<n; i++){
+            int x;
+            cin >> x;
+            arr[i] = x;
+        }
         
+        int sum = v[n-1];
+        if(n>1){
+            for(int i=n-2; i>=0; i--){
+                if(v[i]>arr[i+1]) sum+=(v[i]-arr[i+1]);
+            }
+            cout << sum << "\n";
+
+        }else{
+            cout << v[0] << "\n";
+        }
     }
     return 0;
 }
